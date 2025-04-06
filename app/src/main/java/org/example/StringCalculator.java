8ac755d83a6a62feefdfe046b3e4147126d08b7f
MISABock <Michael.bock999@outlook.com>
2025-04-06 21:01:30 +0200

    zweiter Test Successful

--- app/src/main/java/org/example/StringCalculator.java
+++ app/src/main/java/org/example/StringCalculator.java
@@ -2,7 +2,10 @@
 package org.example;
 
 public class StringCalculator {
+
+
     public int add(String numbers) {
-        return 0; 
+        if (numbers.isEmpty()) return 0;
+        return Integer.parseInt(numbers);
     }
 }
