8623ad50ea8ddbded05f699a837798b18793f41c
MISABock <Michael.bock999@outlook.com>
2025-04-06 21:08:21 +0200

    sechster Test Successful

--- app/src/main/java/org/example/StringCalculator.java
+++ app/src/main/java/org/example/StringCalculator.java
@@ -5,25 +5,25 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-public int add(String numbers) {
-    if (numbers.isEmpty()) return 0;
-
-    String delimiter = "[,\n]";
-    if (numbers.startsWith("//")) {
-        int delimiterIndex = numbers.indexOf("\n");
-        String customDelimiter = numbers.substring(2, delimiterIndex);
-        delimiter = Pattern.quote(customDelimiter);
-        numbers = numbers.substring(delimiterIndex + 1);
-    }
-
-    String[] parts = numbers.split(delimiter);
-    int sum = 0;
-    for (String part : parts) {
-        sum += Integer.parseInt(part);
-    }
-    return sum;
-}
-
+    public int add(String numbers) {
+        if (numbers.isEmpty()) return 0;
     
+        String delimiter = "[,\n]";
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+            String customDelimiter = numbers.substring(2, delimiterIndex);
+            delimiter = Pattern.quote(customDelimiter);
+            numbers = numbers.substring(delimiterIndex + 1);
+        }
     
+        String[] parts = numbers.split(delimiter);
+        int sum = 0;
+        for (String part : parts) {
+            int number = Integer.parseInt(part);
+            if (number <= 1000) {
+                sum += number;
+            }
+        }
+        return sum;
+    }
 }
